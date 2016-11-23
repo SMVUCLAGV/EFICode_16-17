b64d2303aa3ae3a1bde915269da69ac117fe5eb3
jganley <jganley@ucla.edu>
2016-11-22 17:31:35 -0800

    added getCommand implementation

--- EFICode/Controller.h
+++ EFICode/Controller.h
@@ -32,6 +32,44 @@
 
 bool getCommand(); //Check if there is a command in serial, if so, grab it and figure out what function to calculated
 //This is Joey's section
+// send data only when you receive data:
+
+bool getCommand() {
+
+  // send data only when you receive data:
+    if (Serial.available() > 0) {
+      // read the incoming byte:
+      incomingByte = Serial.readString();
+
+      // figure out the command:
+      char firstLetter = incomingByte[0];
+      switch(firstLetter) {
+        case 'i':
+          setIdleVal(incomingByte.substring(5).toFloat());
+          return true;
+        case 'f':
+          setFuelRatio();
+          return true;
+        case 'd':
+          if(incomingByte[1] == 'R') {
+            setDesiredRPM(incomingByte.substring(5).toFloat());
+            return true;
+          }
+          else {
+            setDesiredO2(incomingByte.substring(5).toFloat());
+            return true;
+          }
+        case 'a':
+          setFuelRatioTable(incomingByte.substring(incomingByte[2],incomingByte[4]).toInt(), incomingByte.substring(5).toFloat());
+          return true;
+        case 's':
+          setStartupValue(incomingByte.substring(5).toFloat());
+          return true;
+        default:
+          return false;
+      }
+      return false;
+}
 
 //These functions modify the appropriate parameters
 bool setIdleVal(double val);
